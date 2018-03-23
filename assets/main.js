1e66b55f77686429979bb325d24e14939c5e7970
rafikalid <khalid.rfk@gmail.com>
2018-03-23 18:21:12 +0000

    cc

--- assets/main.js
+++ assets/main.js
@@ -1,57 +1,46 @@
-/**
- * extend for array
- */
-
-function ArrayUtils(array){
-	if(new.target)
-		throw new Error('unsupported operator "new"');
-	else if(Array.isArray(array)){
-		Object.setPrototypeOf(array, ArrayUtilsFactory.prototype);
-	}
-	else throw new Error('The given argument is not an array.');
-	return array;
-}
-
-class ArrayUtilsFactory extends Array{
-
+(function(obj){
+	obj = Object.getOwnPropertyDescriptor(obj);
+	Object.defineProperties(Array.prototype, obj);
+})({
 	/**
 	 * clone an array
 	 * we use "for" instead of "slice" and "push.apply" because it's faster
 	 */
-	clone(){
-		var lst	= new arrayUtils();
+	clone : function(){
+		var lst	= [];
 		for(var i = 0, len = this.length; i < len; ++i)
 			lst.push(this[i]);
 		return lst;
-	}
+	},
+
 	/**
 	 * get first element that matches a condition
 	 * @param {function} 	condition 	desired condition
 	 * @param {int} 		index 		optional start index
 	 * @param {int} 		lastIndex 	optional last index
 	 */
-	first(condition, index = 0, lastIndex){
+	first	: function(condition, index = 0, lastIndex){
 		var len	= this.length;
 		if(len > 0){
 			if(arguments.length === 0)
 				return this[0];
 			else {
-				if(lastIndex && lastIndex < len) len = lastIndex + 1;
+				if(arguments.length > 2 && lastIndex < len) len = lastIndex + 1;
 				if(index < 0) throw new Error('incorrect index: ' + index);
-				while(index < len){
-					if(condition(this[index], index, this))
+				for(;index < len; ++index){
+					if(condition(this[index], index, this) === true)
 						return this[index];
-					++index;
 				}
 			}
 		}
-	}
+	},
+
 	/**
 	 * get the last element that matches a condition
 	 * @param {function} condition desired condition
 	 * @param {int} index last index
 	 */
-	last(condition, beginIndex = 0, lastIndex){
+	last	: function(condition, beginIndex = 0, lastIndex){
 		var len	= this.length - 1;
 		if(len > -1){
 			if(arguments.length === 0)
@@ -65,48 +54,43 @@ class ArrayUtilsFactory extends Array{
 				}
 			}
 		}
-	}
-
-	/** contains */
-	contains(ele){ return this.indexOf(ele) !== -1; }
-
-	push(){super.push.apply(this, arguments); return this;}
-	unshift(){super.unshift.apply(this, arguments); return this;}
-
+	},
 	/**
 	 * each
 	 */
-	each(cb){
+	each	: function(cb){
 		for(var i=0, len = this.length; i < len; ++i){
 			if(cb(this[i], i) === false)
 				break;
 		}
 		return this;
-	}
-
+	},
 	/** remove all elemnts */
-	clear(){
+	clear	: function(){
 		this.splice(0);
 		return this;
-	}
+	},
+	get unique(){
+		return this.filter((ele, indx) => {
+			return this.indexOf(ele) === indx;
+		});
+	},
 
-	eq(index){
+	eq	: function(index){
 		if(index < 0){
 			index += this.length;
 			if(index < 0) return undefined;
 		}
 		return this[index];
 	}
-}
+});
 
-Object.defineProperties(ArrayUtilsFactory.prototype, {
+// contains
+Object.defineProperties(Array.prototype, {
 	contains	: {value: Array.prototype.includes},
 	has			: {value: Array.prototype.includes},
-	duplicate	: {value: ArrayUtilsFactory.prototype.clone}
+	duplicate	: {value: Array.prototype.clone}
 })
-
-// instanceof
-ArrayUtils.prototype = ArrayUtilsFactory.prototype;
 
 // Array.from(arrayLikeElement, mapFunction)
 // Array.copyWithin
